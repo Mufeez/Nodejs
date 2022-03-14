2931694ab67b1a51c3136e3c8d7da0f5e9b03505
Mufeez <mufeez.ahmed22@gmail.com>
2022-03-14 17:05:54 +0530

    Adding illustrations for promise chaining

---
+++ promisechain.js
@@ -0,0 +1,39 @@
+function getCallHistoryUsingNameAPI(name) {
+  // consider that API takes about 2 second to respond
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //phoneNum is hardcoded response for illustration purpose
+      const phoneNum = 1234567890;
+      const phoneDetails = `Your Name is ${name} and Your Number is ${phoneNum}`;
+      console.log(phoneDetails)
+      console.log('First Promise Resolved in 2 seconds')
+      if (name === "unknown") {
+        reject("No Phone Number");
+      } else {
+        resolve(phoneNum);
+      }
+    }, 2000);
+  });
+}
+
+function getCallHistoryUsingPhoneAPI(phoneNum) {
+  // consider that API takes about 2 second to respond
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //history is hardcoded response for illustration purpose
+      const history = ["Monday,Tuesday"];
+      const historyDetails = `History for Phone Number ${phoneNum}  is ${history} ,it took another 2 seconds second promise to resolve`;
+      if (phoneNum === undefined) {
+        reject("No History");
+      } else {
+        resolve(historyDetails);
+      }
+    }, 2000);
+  });
+}
+
+getCallHistoryUsingNameAPI("Mufeez").then((phone) => {
+  getCallHistoryUsingPhoneAPI(phone).then((history) => {
+    console.log(history);
+  });
+});
