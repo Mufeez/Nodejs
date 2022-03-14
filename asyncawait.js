26d168f5ede22927a782aaae69317f65e1d66920
Mufeez <mufeez.ahmed22@gmail.com>
2022-03-14 17:32:47 +0530

    adding Try catch for error handling

--- asyncawait.js
+++ asyncawait.js
@@ -1,15 +1,16 @@
-function getCallHistoryUsingNameAPI(name) {
+function getphoneNumberUsingNameAPI(name) {
     // consider that API takes about 2 second to respond
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         //phoneNum is hardcoded response for illustration purpose
         const phoneNum = 1234567890;
         const phoneDetails = `Your Name is ${name} and Your Number is ${phoneNum}`;
-        console.log(phoneDetails)
-        console.log('First Promise Resolved in 2 seconds')
-        if (name === "unknown") {
+       
+        if (name === undefined) {
           reject("No Phone Number");
         } else {
+          console.log(phoneDetails)
+          console.log('First Promise Resolved in 2 seconds')
           resolve(phoneNum);
         }
       }, 2000);
@@ -33,14 +34,19 @@ function getCallHistoryUsingNameAPI(name) {
   }
 
   async function getHistory(contact) {
+try{
 
-
-    const phone =await getCallHistoryUsingNameAPI(contact);
+    const phone =await getphoneNumberUsingNameAPI(contact);
 
     const history = await getCallHistoryUsingPhoneAPI(phone)
 
     return history
+  }
+  catch(err){
+    
+    console.log(err)
 
+  }
   }
 
   getHistory('Mufeez').then((history)=>{console.log(history)});
--- eventloop.js
+++ eventloop.js
@@ -1,7 +1,7 @@
 console.log('start')
 setTimeout(()=>{
     console.log('called back after 2 second by eventloop from call back queue')
-})
+},2000)
 //callback will be registered in callback q by DOM API after 2 seconds
 //eventloop will push the code in execution context from callback q
 console.log('End')
